42b02f90ce93133fdbc4575ed35da496e0579d71
reloadanam-cloud <reloadanam@gmail.com>
2026-08-13 10:35:09 +0700

    Update text.lua

--- text.lua
+++ text.lua
@@ -107,7 +107,7 @@ getgenv().UserConfig = {
     ["Webhook Note"] = "KAIDEN",
     ["Discord ID"] = "1079411947348439200",
 	-- Mail
-    ["Mail To Username"] = {"xKaiidenn", "xkaiden564", "xkaiden563"},
+    ["Mail To Username"] = {"xKaiidenn", "xkaiden562", "xkaiden564", "xkaiden563"},
     ["Items To Mail"] = {
 		["Pet"] = {
              ["Squirrel"] = {Huge = 1, Rainbow = 1},
